c336b9a23be807e90fec2d6bccdc8e42ed630063
Maarten van Gompel <proycon@anaproy.nl>
2017-06-09 13:34:44 +0200

    first functional version

--- anavec.py
+++ anavec.py
@@ -2,23 +2,25 @@
 import sys
 import os
 import argparse
+from collections import defaultdict
 import theano
 import theano.tensor as T
 import numpy as np
 import colibricore
 
 
-def randomMatrix(n, f):
-    return np.random.randint(6, size=n*f).astype(np.int8).reshape((n, f))
+UNKFEATURE = -1
+PUNCTFEATURE = -1
 
 
 def compute(trainingdata, testdata):
     # adapted from https://gist.github.com/danielvarga/d0eeacea92e65b19188c
     # with lamblin's workaround at https://github.com/Theano/Theano/issues/1399
 
-    n = 10 # number of candidates
-    m = 3 # number of targets
-    f = 50  # number of features
+    n = trainingdata.shape[0] # number of candidates
+    assert testdata.shape[1] == trainingdata.shape[1]
+    m = testdata.shape[0] # number of targets
+    f = testdata.shape[1] # number of features
 
     x = T.matrix('x') # candidates
     y = T.matrix('y') # targets
@@ -29,11 +31,6 @@ def compute(trainingdata, testdata):
     yL2SM = T.zeros((n, m)) + yL2S # # broadcasting, [n, m]
     squaredPairwiseDistances = xL2SM.T + yL2SM - 2.0*T.dot(x, y.T) # [n, m]
 
-    np.random.seed(1)
-
-    N = randomMatrix(n, f)
-    M = randomMatrix(m, f)
-
     lamblinsTrick = False
 
     if lamblinsTrick:
@@ -42,25 +39,20 @@ def compute(trainingdata, testdata):
     else:
         bestIndices = T.argmin(squaredPairwiseDistances, axis=0)
 
-    print("N", N)
-    print("M", M)
-
     nearests_fn = theano.function([x, y], bestIndices, profile=False)
 
     return nearests_fn(trainingdata, testdata)
 
-UNKFEATURE = -1
-PUNCTFEATURE = -1
 
-def buildfeaturevector(word, alphabetmap, numfeatures):
+def buildfeaturevector(word, alphabetmap, numfeatures, args):
     featurevector = np.zeros(numfeatures, dtype=np.uint8)
     for char in word:
         if not char.isalnum():
-            featurevector[PUNCTFEATURE] += 1
+            featurevector[PUNCTFEATURE] += 1 * args.punctweight
         elif char in alphabetmap:
             featurevector[alphabetmap[char]] += 1
         else:
-            featurevector[UNKFEATURE] += 1
+            featurevector[UNKFEATURE] += 1 * args.unkweight
     return featurevector
 
 
@@ -68,8 +60,8 @@ def main():
     parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-m','--patternmodel', type=str,help="Pattern model of background corpus (training data)", action='store',default="",required=True)
     parser.add_argument('-c','--classfile', type=str,help="Class file of background corpus", action='store',default="",required=True)
-    parser.add_argument('-u','--unkweight', type=float,help="Unknown character weight", action='store',default=1,required=True)
-    parser.add_argument('-p','--puncweight', type=float,help="Punctuation character weight", action='store',default=1,required=True)
+    parser.add_argument('-u','--unkweight', type=float,help="Unknown character weight", action='store',default=1,required=False)
+    parser.add_argument('-p','--punctweight', type=float,help="Punctuation character weight", action='store',default=1,required=False)
     parser.add_argument('-d', '--debug',action='store_true')
     args = parser.parse_args()
 
@@ -81,7 +73,7 @@ def main():
         sys.exit(2)
 
     print("Test input words, one per line (if interactively invoked, type ctrl-D when done)",file=sys.stderr)
-    testwords = sys.stdin.readlines()
+    testwords = [ w.strip() for w in sys.stdin.readlines() ]
 
     numtest = len(testwords)
     print("Test set size: ", numtest, file=sys.stderr)
@@ -90,7 +82,7 @@ def main():
     classdecoder = colibricore.ClassDecoder(args.classfile)
     patternmodel = colibricore.UnindexedPatternModel(args.patternmodel)
 
-    alphabet = {}
+    alphabet = set()
 
     numtraining = 0 #number of word types
     print("Computing alphabet on training data...",file=sys.stderr)
@@ -98,8 +90,8 @@ def main():
         if len(pattern) == 1: #only unigrams for now
             numtraining += 1
             word = pattern.tostring(classdecoder) #string representation
-            for char in word and char not in alphabet:
-                if char.isalpha():
+            for char in word:
+                if char.isalpha() and char not in alphabet:
                     alphabet.add(char)
 
 
@@ -113,21 +105,44 @@ def main():
     if args.debug: print(alphabetmap)
 
     print("Building test vectors...", file=sys.stderr)
-    testdata = np.array( ( buildfeaturevector(testword, alphabetmap, numfeatures ) for testword in testwords) )
+    testdata = np.array( [ buildfeaturevector(testword, alphabetmap, numfeatures, args ) for testword in testwords] )
     if args.debug: print("[DEBUG] TEST DATA: ", testdata)
 
     print("Building training vectors...", file=sys.stderr)
     trainingdata = np.empty((numtraining, numfeatures), dtype=np.int8)
 
-    index = 0
+    instanceindex = 0
     for pattern in patternmodel:
         if len(pattern) == 1: #only unigrams for now
-            trainingdata[index] = buildfeaturevector(pattern.tostring(classdecoder), alphabetmap, numfeatures)
-            index  += 1
+            trainingdata[instanceindex] = buildfeaturevector(pattern.tostring(classdecoder), alphabetmap, numfeatures, args)
+            instanceindex  += 1
 
     if args.debug: print("[DEBUG] TRAINING DATA: ", trainingdata)
 
-    result = compute(trainingdata, testdata)
+    print("Computing matches...", file=sys.stderr)
+    bestindices = compute(trainingdata, testdata)
+
+    print("Resolving results...", file=sys.stderr)
+    bestindexmap = defaultdict(set)
+    for bestindex, testword in zip(bestindices, testwords):
+        bestindexmap[bestindex].add(testword)
+
+    solutions = {} #maps test words to solutions (the best match)... (strings for now)
+
+    instanceindex = 0
+    for pattern in patternmodel:
+        if len(pattern) == 1: #only unigrams for now
+            if instanceindex in bestindexmap:
+                #TODO: this is the place for extra score computation and thresholding
+                for testword in bestindexmap[instanceindex]:
+                    solutions[testword] = pattern.tostring(classdecoder)
+            instanceindex += 1
+
+    print("Outputting...", file=sys.stderr)
+    #output in same order as input
+    for testword in testwords:
+        print(testword + "\t" + solutions[testword])
+
 
 if __name__ == '__main__':
     main()
